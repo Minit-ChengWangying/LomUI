e127b0fadb4528fbb0e05ccf1d31eb075ff97066
Minit-ChengWangying <10096014+wangmaixin@user.noreply.gitee.com>
2022-08-26 12:03:39 +0800

    version 1.0.1

--- js/Popup.js
+++ js/Popup.js
@@ -1,22 +1,18 @@
 var BackgroundColor = $('body').css('background-color');
-if(BackgroundColor == 'rgb(255, 255, 255)') {
+// console.log(BackgroundColor);
+if(BackgroundColor == 'rgba(0, 0, 0, 0)') {
     $('.pop_container').css('background-color','#f5f5f5');
 }
 // 针对纯白页面的适应
 
-// 弹出层
-function Popup(PopTextFun) {
-    if(PopTextFun == null) {
-        console.log("Err:Use function without parameter set!");
-        return false;
-    }
-    var PopContainer = $('.pop_container');
-    var PopText = $(".popText");
-    PopContainer.css('top','10px');
-    PopText.text(PopTextFun);
 
-    setTimeout(function(){
-        PopContainer.css('top','-55px');
-    }, 3000);   // 在此处设置弹出层弹出时间
+function toggle() {
+    var blur = document.getElementById('popup');
+    blur.classList.toggle('active');
+    setTimeout(function() {
+        var blur = document.getElementById('popup');
+        blur.classList.toggle('active');
+    },3000);
+    
 }
 //此函数使用必须传参
